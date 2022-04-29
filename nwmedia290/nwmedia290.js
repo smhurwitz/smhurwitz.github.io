096168625bd37679cab49b722e7a5f0123ab166b
Lauren Marietta Chambers <lauren.m.chambers96@gmail.com>
2022-04-29 16:34:09 -0700

    Mess around with video settings

--- nwmedia290/nwmedia290.js
+++ nwmedia290/nwmedia290.js
@@ -4,10 +4,6 @@ window.onload = function () {
   $('#selectRace').change(changeIntroPortrait);
   $('#selectGender').change(changeIntroPortrait);
 
-  // $('#continueButton').click(loadSpeech)
-
-  // $('#new_exec_link').click(loadSection(event))
-
 };
 
 var audio = new Audio('audio/mixkit-negative-answer-lose-2032.wav');
@@ -21,34 +17,7 @@ changeIntroPortrait = function(){
     selectedRace = $("#selectRace").val();
 
     var portrait_filename = "images/" + selectedRace + "_" + selectedGender + ".jpg"
-    $('#introPortrait')[0].src = portrait_filename
-
-
-
-    // if (selectedGender == "Woman") {
-    //   if (selectedRace == "Black") {
-    //     $('#introPortrait')[0].src = "images/black-woman.jpg"
-    //     // video1 = "/images/nwmedia290/videos/black-woman-1.mp4"
-    //   } else if (selectedRace == "White") {
-    //     $('#introPortrait')[0].src = "images/white-woman.jpg"
-    //   } else if (selectedRace == "Latinx") {
-    //     $('#introPortrait')[0].src = "images/latinx-woman.jpg"
-    //   } else if (selectedRace == "Asian") {
-    //     $('#introPortrait')[0].src = "images/asian-woman.jpg"
-    //   }
-    // } else if (selectedGender == "Man") {
-    //   if (selectedRace == "Black") {
-    //     $('#introPortrait')[0].src = "images/black-man.jpg"
-    //   } else if (selectedRace == "White") {
-    //     $('#introPortrait')[0].src = "images/white-man.jpg"
-    //   } else if (selectedRace == "Latinx") {
-    //     $('#introPortrait')[0].src = "images/latinx-man.jpg"
-    //   } else if (selectedRace == "Asian") {
-    //     $('#introPortrait')[0].src = "images/asian-man.jpg"
-    //   }
-    // }
-
-    
+    $('#introPortrait')[0].src = portrait_filename    
 
   }
 
@@ -62,7 +31,6 @@ loadSpeech = function(event, back=false) {
     $(speaking_video_id)[0].pause()
   }
 
-  console.log("LOADING SPEECH");
   $('#faceSelect')[0].style.display = "none"
   $('#intro')[0].style.display = "block"
 
@@ -70,7 +38,6 @@ loadSpeech = function(event, back=false) {
     // Hide all other sections
     for (const id of all_sections_no_intro) {
       jquery_str = "#" + id;
-      console.log(jquery_str)
       $(jquery_str)[0].style.display = "none";
     }
 
@@ -85,16 +52,13 @@ loadSpeech = function(event, back=false) {
   var video_filename = "videos/" + selectedRace + "_" + selectedGender + "_intro.mov"
 
   var video = document.getElementById('video_intro');
-  var source = document.createElement('source');
+  var source = $('#video_intro source')[0];
 
   source.setAttribute('src', video_filename);
-  source.setAttribute('type', 'video/mp4');
-  video.appendChild(source);
 
   video.load();
 
   $("#video_intro").click(function() {
-    //console.log(this); 
     if (this.paused) {
       this.play();
       speaking_video_id = "#video_intro"
@@ -106,8 +70,11 @@ loadSpeech = function(event, back=false) {
 
   if (back == false) {
     $("#video_intro")[0].play()
+    speaking_video_id = '#video_' + section
   }
 }
+
+const visited_sections = [];
 
 loadSection = function(event, warn=false) {
   // If there is a video playing, pause it
@@ -121,17 +88,27 @@ loadSection = function(event, warn=false) {
   console.log(section)
   section = section.substring(0, section.length - 5); // Remove "_link"
 
+  // Determine if this is the first time we've visited this section
+  console.log(visited_sections)
+  console.log(visited_sections)
+  if (visited_sections.indexOf(section) > -1) {
+    console.log("repeat visit")
+    var visited = true
+  } else {
+    console.log("first visit")
+    var visited = false;
+    visited_sections.push(section);
+    console.log(visited_sections)
+  }
+
   const i_remove = all_sections.indexOf(section);
   const sections_to_hide = all_sections;
   if (i_remove > -1) {
     sections_to_hide.splice(i_remove, 1); // 2nd parameter means remove one item only
   }
 
-  console.log(sections_to_hide)
-
   for (const id of sections_to_hide) {
     jquery_str = "#" + id
-    console.log(jquery_str)
     $(jquery_str)[0].style.display = "none"
   }
 
@@ -148,16 +125,16 @@ loadSection = function(event, warn=false) {
   var video_filename = "videos/" + selectedRace + "_" + selectedGender + "_" + section + ".mov"
 
   var video = document.getElementById('video_' + section);
-  var source = document.createElement('source');
+  // var source = document.createElement('source');
+  var source = $('#video_' + section + " source")[0];
 
   source.setAttribute('src', video_filename);
-  source.setAttribute('type', 'video/mp4');
-  video.appendChild(source);
+  // source.setAttribute('type', 'video/mp4');
+  // video.appendChild(source);
 
   video.load();
 
   $('#video_' + section).click(function() {
-    //console.log(this); 
     if (this.paused) {
       this.play();
       speaking_video_id = '#video_' + section
@@ -166,5 +143,11 @@ loadSection = function(event, warn=false) {
       speaking_video_id = ""
     }
   });
+
+  if (visited == false) {
+    console.log("autoplaying!")
+    video.play()
+    speaking_video_id = '#video_' + section
+  }
 }
 
